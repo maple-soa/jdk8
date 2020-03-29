6c86efe5475d298584945db017cbeff08029ff6e
lijian <371672759@qq.com>
2020-03-29 21:18:10 +0800

    init commit

---
+++ src/com/maple/jdk8/LoopTest.java
@@ -0,0 +1,14 @@
+package com.maple.jdk8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoopTest {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.forEach(item -> {
+            System.out.println(item);
+        });
+    }
+}
